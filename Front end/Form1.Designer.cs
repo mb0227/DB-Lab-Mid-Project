7097712f2787795086516301ab4d9eb5111d1e33
mb0227 <bilalyaseen2k4@gmail.com>
2024-03-20 05:04:36 +0500

    HomePage Made

---
+++ Front end/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Front_end
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            label1 = new Label();
+            button1 = new Button();
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(76, 48);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(136, 126);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(873, 47);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(128, 126);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 2;
+            pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 20F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(340, 86);
+            label1.Name = "label1";
+            label1.Size = new Size(454, 45);
+            label1.TabIndex = 3;
+            label1.Text = "Lab Management System";
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button1.BackColor = Color.Teal;
+            button1.FlatAppearance.BorderColor = Color.Honeydew;
+            button1.FlatAppearance.BorderSize = 5;
+            button1.FlatAppearance.MouseDownBackColor = Color.White;
+            button1.FlatAppearance.MouseOverBackColor = Color.DarkSlateGray;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(419, 293);
+            button1.Name = "button1";
+            button1.Size = new Size(322, 63);
+            button1.TabIndex = 4;
+            button1.Text = "Manage Students";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button2.BackColor = Color.Teal;
+            button2.FlatAppearance.BorderColor = Color.Honeydew;
+            button2.FlatAppearance.BorderSize = 5;
+            button2.FlatAppearance.MouseDownBackColor = Color.White;
+            button2.FlatAppearance.MouseOverBackColor = Color.DarkSlateGray;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(419, 391);
+            button2.Name = "button2";
+            button2.Size = new Size(322, 59);
+            button2.TabIndex = 5;
+            button2.Text = "Manage CLO";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button3.BackColor = Color.Teal;
+            button3.FlatAppearance.BorderColor = Color.Honeydew;
+            button3.FlatAppearance.BorderSize = 5;
+            button3.FlatAppearance.MouseDownBackColor = Color.White;
+            button3.FlatAppearance.MouseOverBackColor = Color.DarkSlateGray;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.ForeColor = Color.White;
+            button3.Location = new Point(419, 482);
+            button3.Name = "button3";
+            button3.Size = new Size(322, 63);
+            button3.TabIndex = 6;
+            button3.Text = "Manage Rubric";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button4.BackColor = Color.Teal;
+            button4.FlatAppearance.BorderColor = Color.Honeydew;
+            button4.FlatAppearance.BorderSize = 5;
+            button4.FlatAppearance.MouseDownBackColor = Color.White;
+            button4.FlatAppearance.MouseOverBackColor = Color.DarkSlateGray;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.ForeColor = Color.White;
+            button4.Location = new Point(419, 574);
+            button4.Name = "button4";
+            button4.Size = new Size(322, 61);
+            button4.TabIndex = 7;
+            button4.Text = "Manage Assesment";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            button5.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button5.BackColor = Color.Teal;
+            button5.FlatAppearance.BorderColor = Color.Honeydew;
+            button5.FlatAppearance.BorderSize = 5;
+            button5.FlatAppearance.MouseDownBackColor = Color.White;
+            button5.FlatAppearance.MouseOverBackColor = Color.DarkSlateGray;
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.ForeColor = Color.White;
+            button5.Location = new Point(419, 668);
+            button5.Name = "button5";
+            button5.Size = new Size(322, 60);
+            button5.TabIndex = 8;
+            button5.Text = "Manage Rubric Level\r\n";
+            button5.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 27F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Teal;
+            ClientSize = new Size(1116, 833);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(4, 3, 4, 3);
+            Name = "Form1";
+            Text = "Home";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private Label label1;
+        private Button button1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+    }
+}
---
+++ Front end/Student.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Front_end
+{
+    partial class Student
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox7 = new TextBox();
+            label7 = new Label();
+            textBox2 = new TextBox();
+            label2 = new Label();
+            textBox3 = new TextBox();
+            label3 = new Label();
+            textBox4 = new TextBox();
+            label4 = new Label();
+            textBox5 = new TextBox();
+            label5 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(72, 106);
+            label1.Name = "label1";
+            label1.Size = new Size(118, 27);
+            label1.TabIndex = 0;
+            label1.Text = "First Name";
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(281, 103);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(211, 35);
+            textBox1.TabIndex = 1;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(851, 106);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(211, 35);
+            textBox7.TabIndex = 13;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(681, 109);
+            label7.Name = "label7";
+            label7.Size = new Size(118, 27);
+            label7.TabIndex = 12;
+            label7.Text = "First Name";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(281, 174);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(211, 35);
+            textBox2.TabIndex = 15;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(72, 177);
+            label2.Name = "label2";
+            label2.Size = new Size(117, 27);
+            label2.TabIndex = 14;
+            label2.Text = "Last Name";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(851, 177);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(211, 35);
+            textBox3.TabIndex = 17;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(681, 180);
+            label3.Name = "label3";
+            label3.Size = new Size(118, 27);
+            label3.TabIndex = 16;
+            label3.Text = "First Name";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(281, 256);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(211, 35);
+            textBox4.TabIndex = 19;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(72, 259);
+            label4.Name = "label4";
+            label4.Size = new Size(163, 27);
+            label4.TabIndex = 18;
+            label4.Text = "Registration No";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(851, 251);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(211, 35);
+            textBox5.TabIndex = 21;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(681, 254);
+            label5.Name = "label5";
+            label5.Size = new Size(118, 27);
+            label5.TabIndex = 20;
+            label5.Text = "First Name";
+            // 
+            // Student
+            // 
+            AutoScaleDimensions = new SizeF(13F, 27F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Teal;
+            ClientSize = new Size(1146, 642);
+            Controls.Add(textBox5);
+            Controls.Add(label5);
+            Controls.Add(textBox4);
+            Controls.Add(label4);
+            Controls.Add(textBox3);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox7);
+            Controls.Add(label7);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Margin = new Padding(4, 3, 4, 3);
+            Name = "Student";
+            Text = "Form2";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox7;
+        private Label label7;
+        private TextBox textBox2;
+        private Label label2;
+        private TextBox textBox3;
+        private Label label3;
+        private TextBox textBox4;
+        private Label label4;
+        private TextBox textBox5;
+        private Label label5;
+    }
+}
